06ece4ac742d0931988d30c64e6943cbe0d948cd
Alex Lake-Smith <alakesmi@TVNZ2020644.local>
2022-03-28 13:41:40 +1300

    Push 4 help.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { DriverData } from "./Interfaces/DriversInterface";
+import axios, { AxiosResponse } from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [driverData, setDriverData] = useState<DriverData[]>([]);
+  const drivers: DriverData[] = driverData;
+  //console.log(drivers.MRData.DriverTable.Drivers);
+  //console.log(driverData);
+
+  useEffect(() => {
+    axios
+      .get<DriverData[]>("https://ergast.com/api/f1/2022/drivers.json")
+      .then((response: AxiosResponse) => {
+        console.log(response.data);
+        setDriverData(response.data);
+      });
+  }, []);
+
+  const displayTable = () => {
+    return (
+      <>
+        <body>
+          <tr>
+            <th>Name</th>
+            <th>Number</th>
+            <th>Nationality</th>
+            <th>Birthdate</th>
+          </tr>
+          {console.log(driverData)}
+          {driverData.MRData.DriverTable.Drivers.map((item, key) => (
+            <tr key={key}>
+              <td>{item.driverID}</td>
+            </tr>
+          ))}
+        </body>
+      </>
+    );
+  };
+
+  return <div className="App">Welcome to my F1 API App</div>;
 }
 
 export default App;
---
+++ src/Interfaces/DriversInterface.ts
@@ -0,0 +1,25 @@
+export interface DriverData {
+  xmlns: string;
+  series: string;
+  url: string;
+  limit: string;
+  offset: string;
+  total: string;
+  DriverTable: DriverTable;
+}
+
+export interface DriverTable {
+  season: string;
+  Drivers: Drivers[];
+}
+
+export interface Drivers {
+  driverId: string;
+  permanentNumber: string;
+  code: string;
+  url: string;
+  givenName: string;
+  familyName: string;
+  dateOfBirth: string;
+  nationality: string;
+}

79eefc322f8b645d589763c0840a566ed77da625
Amanda <amanda.hogan@tvnz.co.nz>
2022-03-28 15:41:19 +1300

    display drivers names

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,39 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { DriverData } from "./Interfaces/DriversInterface";
+import { DriverData, Drivers } from "./Interfaces/DriversInterface";
 import axios, { AxiosResponse } from "axios";
 
 function App() {
-  const [driverData, setDriverData] = useState<DriverData[]>([]);
-  const drivers: DriverData[] = driverData;
-  //console.log(drivers.MRData.DriverTable.Drivers);
-  //console.log(driverData);
+  // const [driverData, setDriverData] = useState<DriverData[]>([]);
+  const [drivers, setDrivers] = useState<Drivers[]>([]);
 
   useEffect(() => {
     axios
       .get<DriverData[]>("https://ergast.com/api/f1/2022/drivers.json")
       .then((response: AxiosResponse) => {
-        console.log(response.data);
-        setDriverData(response.data);
+        // console.log('1111', response.data);
+        setDrivers(response.data.MRData.DriverTable.Drivers);
       });
   }, []);
 
-  const displayTable = () => {
-    return (
-      <>
-        <body>
-          <tr>
-            <th>Name</th>
-            <th>Number</th>
-            <th>Nationality</th>
-            <th>Birthdate</th>
-          </tr>
-          {console.log(driverData)}
-          {driverData.MRData.DriverTable.Drivers.map((item, key) => (
-            <tr key={key}>
-              <td>{item.driverID}</td>
-            </tr>
-          ))}
-        </body>
-      </>
-    );
-  };
+  return (
+    <>
+      <div className="App">
+        Welcome to my F1 API App
+      </div>
+      <tr>
+        <th>Name</th>
+      </tr>
+    {/* {console.log({drivers})} */}
+    {drivers.map((driver) => (
+      <tr key={driver.driverId}>
+        <td>{driver.driverId}</td>
+      </tr>
+    ))}
+    </>
+  )
 
-  return <div className="App">Welcome to my F1 API App</div>;
 }
 
 export default App;

0ad3de334eca5ffd2349c68eadd09293b7f2f4b5
Alex Lake-Smith <alakesmi@TVNZ2020644.local>
2022-03-30 12:14:02 +1300

    Going to clean up the Drivers info fetching and then start building different pages for each team with relevant info.

--- src/Components/Drivers.tsx
+++ src/Components/Drivers.tsx
@@ -4,41 +4,9 @@ import axios, { AxiosResponse } from "axios";
 import { DriverData, Drivers } from "../Interfaces/DriversInterface";
 
 const DriversPage = () => {
-  const [driverData, setDriverData] = useState<Drivers[]>([]);
-
-  useEffect(() => {
-    axios
-      .get<DriverData[]>("https://ergast.com/api/f1/2022/drivers.json")
-      .then((response: AxiosResponse) => {
-        setDriverData(response.data.MRData.DriverTable.Drivers);
-      });
-  }, []);
-
-  const DisplayResults = () => {
-    return (
-      <>
-        <div className="driversTableData">
-          <tr>
-            <th>Name</th>
-          </tr>
-
-          {driverData.map((driver) => (
-            <div className="eachDriver">
-              <tr key={driver.driverId}>
-                <td className="firstName">{driver.givenName}</td>
-                <td className="lastName">{driver.familyName}</td>
-              </tr>
-            </div>
-          ))}
-        </div>
-      </>
-    );
-  };
-
   return (
     <>
       <div>This is the drivers page</div>
-      <DisplayResults />
     </>
   );
 };
